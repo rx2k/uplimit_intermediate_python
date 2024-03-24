d96e07a7f430829316444314b01b9b6ba124e395
Raphael Ver <32309759+rx2k@users.noreply.github.com>
2024-03-24 17:46:56 +0000

    added logger w4

--- w4/logger_config.py
+++ w4/logger_config.py
@@ -18,33 +18,27 @@ class Logger:
         self.f_handler = logging.FileHandler(os.path.join(CURRENT_FOLDER_NAME, 'logs', log_file_name))
 
         # Create formatters and add it to handlers
-        ######################################## YOUR CODE HERE ##################################################
-        # set the logging formatter to the f_handler
-        ######################################## YOUR CODE HERE ##################################################
 
-        ######################################## YOUR CODE HERE ##################################################
+        # set the logging formatter to the f_handler
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.f_handler.setFormatter(formatter)
+
+        
         # Add handlers to the logger and setlevel to DEBUG
-        ######################################## YOUR CODE HERE ##################################################
+        self.logger.addHandler(self.f_handler)
+        self.logger.setLevel(logging.DEBUG)
 
     def warning(self, msg):
-        pass
-        ######################################## YOUR CODE HERE ##################################################
-        ######################################## YOUR CODE HERE ##################################################
+        self.logger.warning(msg=msg)
 
     def error(self, msg):
-        pass
-        ######################################## YOUR CODE HERE ##################################################
-        ######################################## YOUR CODE HERE ##################################################
+        self.logger.error(msg=msg)
 
     def info(self, msg):
-        pass
-        ######################################## YOUR CODE HERE ##################################################
-        ######################################## YOUR CODE HERE ##################################################
+        self.logger.info(msg=msg)
 
     def debug(self, msg):
-        pass
-        ######################################## YOUR CODE HERE ##################################################
-        ######################################## YOUR CODE HERE ##################################################
+        self.logger.debug(msg=msg)
 
 
 server_logger = Logger(log_file_name='server_logs.txt', module_name='server_logs')
